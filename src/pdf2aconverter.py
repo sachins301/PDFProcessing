6a1054db73ad21954e890d15fb04b099a8e8d8ae
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-06-07 11:00:01 -0600

    gs errors fixed

--- src/pdf2aconverter.py
+++ src/pdf2aconverter.py
@@ -5,16 +5,11 @@ def convert_to_pdfa(input_pdf, output_pdf):
     gs_command = [
         r"D:\Projects\PDFProcessing\packages\gs10.03.1\bin\gswin64c.exe",
         "-dPDFA=2",
-        "-dBATCH",
-        "-dNOPAUSE",
-        "-sProcessColorModel=DeviceCMYK",
         "-sDEVICE=pdfwrite",
-        "-sColorConversionStrategy=CMYK",
-        "-sProcessColorModel=DeviceCMYK",
-        f"-sOutputICCProfile={icc_profile_path}",  # Path to the ICC profile
-        "-sPDFACompatibilityPolicy=1",
-        f"-sOutputFile={output_pdf}",
-        input_pdf
+        "-sColorConversionStrategy=UseDeviceIndependentColor",
+        "-dPDFACompatibilityPolicy=1",
+        f"-o{output_pdf}",
+        f"-f{input_pdf}"
     ]
     try:
         subprocess.run(gs_command, check=True)
